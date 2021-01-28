d353d3403a3837bb28052bec7cdc912ebe674249
Erik Poole <erikjpoole@gmail.com>
2021-01-28 14:33:59 -0700

    cleaning up filenames

--- 1_stock_1_scraper.py
+++ 1_stock_1_scraper.py
@@ -7,35 +7,44 @@ import sys
 BASE_URL = "http://www.1stock1.com"
 OUTPUT_FOLDER = "output"
 
+CONSECTUTIVE_FAILURE_LIMIT = 200
+
 def main():
-    # probably don't fail for everything...
-    # store invalid request pages in log file
-    for value in range(1):
-        targetted_url = f"{BASE_URL}/1stock1_148.htm"
+    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+
+    count = 1
+    consecutive_failures = 0
+    while consecutive_failures < CONSECTUTIVE_FAILURE_LIMIT:
+        targetted_url = f"{BASE_URL}/1stock1_2582.htm"
+        count += 1
         try:
             raw_page = requests.get(targetted_url)
             soup = BeautifulSoup(raw_page.content, "html.parser")
 
-            title = get_title(soup)
-            if title == "":
-                print("WARNING: file without title")
+            stock_name = get_stock_name(soup)
+            file_name = create_file_name(stock_name)
 
             returns_table = get_returns_table(soup)
             returns = get_returns(returns_table)
             output_text = parse_returns(returns)
 
-            os.makedirs(OUTPUT_FOLDER, exist_ok=True)
-            create_file(f"{title}.txt", output_text)
+            create_file(file_name, output_text)
+            consecutive_failures = 0
 
         except Exception as error:
             print(f"{targetted_url} failed; {error}")
+            consecutive_failures += 1
 
+        break
 
-
-def get_title(soup):
+def get_stock_name(soup):
     title_indicator = " Yearly Returns"
     raw_title = soup.find(text=re.compile(title_indicator))
-    return raw_title[:raw_title.find(title_indicator)]
+    if raw_title == None:
+        raise Exception("page does not match stock history format")
+
+    raw_stock_name = raw_title[:raw_title.find(title_indicator)]
+    return raw_stock_name.strip()
 
 def get_returns_table(soup):
     for table_element in soup.find_all("table"):
@@ -61,10 +70,18 @@ def parse_returns(returns):
     
     return parsed
 
+def create_file_name(text):
+    invalid_chars = ["\\", "/", ":", "*", "?", "/", "<", ">", "|"]
+
+    cleaned_text = text
+    for invalid_char in invalid_chars:
+        cleaned_text = cleaned_text.replace(invalid_char, " ")
+    return f"{cleaned_text}.txt"
+
 def create_file(file_name, text):
     working_dir = os.path.dirname(os.path.realpath(__file__))
     output_path = os.path.join(working_dir, OUTPUT_FOLDER, file_name)
-    with open(f"output/{file_name}", "w") as file:
+    with open(output_path, "w") as file:
         file.write(text)
 
 
